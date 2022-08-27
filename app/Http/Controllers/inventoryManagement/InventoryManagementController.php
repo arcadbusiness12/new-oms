1785c47fcbbef00913001e18d69a21cac5b0a90e
Businessarcade <alisiraj346@gmail.com>
2022-08-27 15:31:07 +0400

    done

--- app/Http/Controllers/inventoryManagement/InventoryManagementController.php
+++ app/Http/Controllers/inventoryManagement/InventoryManagementController.php
@@ -1004,10 +1004,11 @@ use Carbon\Carbon;
   }
 
   public function optionConnection(Request $request) {
-    // $baOption = OptionDescriptionModel::groupBy('option_id')->where('language_id', 1)->orderBy('name')->get();
-    // $dfOption = DressFairOptionDescriptionModel::groupBy('option_id')->where('language_id', 1)->orderBy('name')->get();
-    $oms_options = OmsOptions::with('omsOptionsDetails')->where('status',1)->groupBy('option_name')->orderBy('option_name')->select(DB::raw('*'))->get();
-    dd($oms_options->toArray());
+    // ->select(DB::raw('*'))
+    $baOption = OptionDescriptionModel::where('language_id', 1)->orderBy('name')->get();
+    $dfOption = DressFairOptionDescriptionModel::groupBy('option_id')->where('language_id', 1)->orderBy('name')->get();
+    $oms_options = OmsOptions::with('omsOptionsDetails')->where('status',1)->orderBy('option_name')->get();
+    // dd($oms_options->toArray());
     if($request->isMethod('post')) {
 
 
