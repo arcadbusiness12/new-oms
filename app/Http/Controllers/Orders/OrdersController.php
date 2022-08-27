f4032a9b627f3c3a1fc430bf2a697f33b0e9522d
nasir <nasir5074@gmail.com>
2022-08-27 16:51:52 +0400

    changes

--- app/Http/Controllers/Orders/OrdersController.php
+++ app/Http/Controllers/Orders/OrdersController.php
@@ -2,13 +2,37 @@
 
 namespace App\Http\Controllers\Orders;
 use App\Http\Controllers\Controller;
-
+use App\Models\DressFairOpenCart\Orders\OrdersModel AS DFOrdersModel;
+use App\Models\Oms\OmsOrdersModel;
+use App\Models\OpenCart\Orders\OrdersModel;
+use DB;
 class OrdersController extends Controller
 {
 	const VIEW_DIR = 'orders';
     function __construct(){
     }
     public function index(){
-        return view(self::VIEW_DIR.".index");
+        $ba_orders = OrdersModel::with(['status', 'orderd_products','orderd_totals'=>function($query){
+                $query->whereNotIn('code',['tax']);
+             }])
+            ->select(DB::raw("order_id,firstname,lastname,telephone,alternate_number,email,total,shipping_address_1,shipping_address_2,shipping_city,shipping_zone,date_added,date_modified"))
+            // ->orderBy(OrdersModel::FIELD_DATE_MODIFIED, 'desc')
+            ->where('order_type', 'like', 'normal')
+            ->where('order_status_id', '>', 0)
+            ->where('online_approved', 1)
+            ->where('reseller_approve', 1);
+        //dressfair orders
+        $df_orders = DFOrdersModel::with(['status', 'orderd_products','orderd_totals'=>function($query){
+            $query->whereNotIn('code',['tax']);
+            }])
+            ->select(DB::raw("order_id,firstname,lastname,telephone,alternate_number,email,total,shipping_address_1,shipping_address_2,shipping_city,shipping_zone,date_added,date_modified"))
+            // ->orderBy(OrdersModel::FIELD_DATE_MODIFIED, 'desc')
+            ->where('order_type', 'like', 'normal')
+            ->where('order_status_id', '>', 0)
+            ->where('online_approved', 1);
+        $data = $ba_orders->union($df_orders)->orderBy(OrdersModel::FIELD_DATE_MODIFIED, 'desc');
+        $data = $ba_orders->paginate(20);
+        // dd($data->toArray());
+        return view(self::VIEW_DIR.".index",compact('data'));
     }
 }
--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -5,24 +5,299 @@
 <div class="container-fluid relative animatedParent animateOnce my-3">
     <div class="row row-eq-height my-3 mt-3">
         <div class="col-md-12">
+            <div class="row mb-4">
+                <div class="col-md-12 col-sm-12">
+                    <div class="card no-b">
+                        <div class="card-header white">
+                            <form name="form_stock_level" id="form_stock_level" action="" method="get">
+                                {{ csrf_field() }}
+                                <div class="row">
+                                <div class="col-md-2">
+                                <input id="product_sku" name="product_sku" list="product_skus" value="{{ @$old_input['product_sku'] }}" type="text" placeholder="Search By SKU" class="form-control" autocomplete="off">
+                                <datalist id="product_skus"></datalist>
+                                </div>
+                                <div class="col-md-2">
+                                <input id="product_model" name="product_model" list="product_models" value="{{@$old_input['product_model']}}" type="text" placeholder="Search By Model" class="form-control" autocomplete="off">
+                                <datalist id="product_models"></datalist>
+                                </div>
+                                <div class="col-md-2">
+                                <input type="text" class="form-control" name="sku_range_from" value="{{ @$old_input['sku_range_from'] }}" placeholder="Range From">
+                                </div>
+                                <div class="col-md-2">
+                                <input type="text" class="form-control" name="sku_range_to" value="{{ @$old_input['sku_range_to'] }}" placeholder="Range To">
+                                </div>
+                                <div class="col-md-2">
+                                <select name="bystatus" id="bystatus" class="form-control">
+                                    <option value="">Search By Status</option>
+                                    <option value="0" {{ (@$old_input['bystatus']=='0') ? "selected" : "" }}>Disable</option>
+                                    <option value="1" {{ (@$old_input['bystatus']=='1') ? "selected" : "" }}>Enable</option>
+                                    <option value="2" {{ (@$old_input['bystatus']=='2') ? "selected" : "" }}>Finished</option>
+                                </select>
+                                </div>
+                                <div class="col-md-2">
+                                <select name="by_type" id="by_type" class="form-control">
+                                    <option value="">Search By Status</option>
+                                    {{-- @forelse($product_types as $key => $row)
+                                    <option value="{{ $row->id }}" {{ (@$old_input['by_type']== $row->id ) ? "selected" : "" }} >{{ $row->name }}</option>
+                                    @empty
+                                    @endforelse --}}
+                                </select>
+                                </div>
+                            </div>
+                            <br>
+                            <div class="row">
+                                <div class="col-md-3 pull-right">
+                                <input type="submit" name="search_inv_dashboard" class="btn btn-sm btn-primary" value="Search">
+                                <button type="submit" id="excel_export" name="generate_csv" value="generate_csv" class="btn btn-warning">Export</button>
+                                @if( session('user_group_id') == 1 )
+                                    {{-- <a href="{{ route('inventory_manage.inventorySyncReport') }}" class="btn btn-success">Sync Report</a> --}}
+                                @endif
+                                </div>
+                            </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <div class="row">
                 <div class="col-md-12 col-sm-12">
                     <div class="card no-b">
-                        <div class="panel-heading">All Orders</div>
-                        @if(session()->has('success'))
-                        <div role="alert" class="alert alert-success">
+                        {{--  <div class="panel-heading">Inventory Dashboard</div>  --}}
+                          @if(session()->has('success'))
+                            <div role="alert" class="alert alert-success">
                             {{ session()->get('success') }}
-                        </div>
-                        @endif
-                        <div class="card-header white">
-                            <h1>Testing...</h1>
-                        </div>
-                    </div> {{--  end class card no-b  --}}
+                            </div>
+                          @endif
+
+                          <div class="table-responsive">
+                           <div id="status_changed_msg" style="display: none"></div>
+                            <table class="table" width="100%" style="font-size: 13px !important">
+                             <thead>
+                              <tr style="background-color: #3f51b5;color:white">
+                                <th scope="col"><center>Order Id</center></th>
+                                <th scope="col"><center>Customer</center></th>
+                                <th scope="col"><center>Status</center></th>
+                                <th scope="col"><center>Date Added</center></th>
+                                <th scope="col"><center>Date Modified</center></th>
+                                <th scope="col"><center>Telephone</center></th>
+                                <th scope="col"><center>Email</center></th>
+                                <th scope="col"><center>Total</center></th>
+                               </tr>
+                             </thead>
+                             <tbody>
+                                @if($data->count())
+                                @foreach($data as $key=>$order)
+                                    <tr>
+                                        <td class="col-sm-1"><center>{{ $order->order_id }}</center></td>
+                                        <td class="column col-sm-1 td-valign"><center>{{ $order->firstname }} {{ $order->lastname }}</center></td>
+                                        <td class="column col-sm-1 td-valign"><center>-</center></td>
+                                        <td class="column col-sm-1 td-valign"><center>{{ $order->date_added }} </center></td>
+                                        <td class="column col-sm-1 td-valign"><center>{{ $order->date_modified }} </center></td>
+                                        <td class="column col-sm-1 td-valign"><center>{{ $order->telephone }} </center></td>
+                                        <td class="column col-sm-1 td-valign"><center>{{ $order->email }} </center></td>
+                                        <td class="column col-sm-1 td-valign"><center>{{ $order->total }} </center></td>
+                                    </tr>
+                                    <tr>
+                                        <td>&nbsp;</td>
+                                        <td colspan="3"><strong>Address:</strong>{{ $order->payment_area }},{{ $order->payment_address_1 }},{{ $order->shipping_address_2 }}</td>
+                                        <td colspan="2"><strong>City:</strong>{{ $order->shipping_city ? $order->shipping_city : $order->shipping_zone }}</td>
+                                        <td colspan="3"></td>
+                                    </tr>
+                                    @if( $order->orderd_products )
+                                        <tr style="border-bottom: 7px solid #e9e9e9 !important">
+                                            <td colspan="8">
+                                                <center>
+                                                <table width="100%" style="font-size:12px;">
+                                                    @foreach ( $order->orderd_products as $ordered_product )
+                                                        <tr>
+                                                            <td>&nbsp;</td>
+                                                            <td>&nbsp;</td>
+                                                            <td>&nbsp;</td>
+                                                            <td>&nbsp;</td>
+                                                            <td>&nbsp;</td>
+                                                            <td><img src="{{ $ordered_product?->product_details?->image }}" /></td>
+                                                            <td>{{ $ordered_product->name }}<br>
+                                                                @forelse ($ordered_product->order_options as $order_option )
+                                                                    <strong>{{ $order_option->name }} :</strong> {{ $order_option->value }}
+                                                                @empty
+                                                                @endforelse
+                                                            </td>
+                                                            <td>{{ $ordered_product->model }}</td>
+                                                            <td>{{ $ordered_product->quantity }}</td>
+                                                            <td>{{ $ordered_product->price }}</td>
+                                                            <td>{{ $ordered_product->total }}</td>
+                                                        </tr>
+                                                    @endforeach
+                                                </table>
+                                                </center>
+                                            </td>
+                                        </tr>
+                                    @endif
+                                @endforeach
+                                @endif
+                             </tbody>
+                            </table>
+                        {{  $data->appends(@$old_input)->render() }}
+                    </div>
+
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
 
+<div class="toast-action" data-title="Hey, Bro!" data-message="Paper Panel has toast as well." data-type="success" data-position-class="toast-top-right"></div>
+@include('inventoryManagement.dashboardModals')
+<style>
+.table td {
+    border-top: none !important;
+}
+thead, tbody, tfoot, tr, td, th {
+    border: none !important;
+}
+</style>
 @endsection
 
+@push('scripts')
+<script>
+    function changeProductStatusAjax(status,product_id){
+        $.ajax({
+            url: '{{route("change.product.status")}}',
+            type: 'POST',
+            headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+            data: 'product_id='+product_id+'&status='+status,
+            success: function (data) {
+                if (data['status']) {
+                    $(".toast-action").data('title', 'Action Done!');
+                    $(".toast-action").data('type', 'success');
+                    $(".toast-action").data('message', data['msg']);
+                    $(".toast-action").trigger('click');
+                } else {
+                    $(".toast-action").data('title', 'Went Wrong!');
+                    $(".toast-action").data('type', 'error');
+                    $(".toast-action").data('message', data['msg']);
+                    $(".toast-action").trigger('click');
+                }
+            }
+        });
+  }
+
+  function viewInventory(sku){
+
+    $('#history-tbl').css('display', 'none');
+      var url = "{{ route('view.inventory.product.details', ':sku') }}";
+      url     = url.replace(':sku', sku);
+      console.log(url);
+    $.ajax({
+      type: 'GET',
+      url: url,
+      headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+      cache: false,
+      success: function (data) {
+        $('#porduct_view_content').html(data);
+      }
+    });
+  }
+
+  $('.history-btn').on('click', function() {
+    // console.log($("input[name=product_id]").val());
+    $('table .history').html('');
+    $('.history-load').css('display', 'block');
+    var id = $("input[name=product_id]").val();
+    // $('.history').html("<b><center>Loaidng...</center></b>");
+    console.log(id);
+    var url = "{{route('inventory.product.history', ':id')}}";
+    url = url.replace(':id', id);
+    $.ajax({
+      url: url,
+      type: 'GET',
+      headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+      cache: false,
+      success: function(history) {
+      $('.history-load').css('display', 'none');
+        if(history.status){
+          var html = '';
+          $('#history-tbl').css('display', 'block');
+            history.history.forEach(function(v) {
+              var full_name = "";
+              if( v.user ){
+                full_name = v.user.firstname+" "+v.user.lastname;
+              }
+            html += '<tr><td class="text-center">'+full_name+'</td><td class="text-center">'+v.comment+'</td><td class="text-center">'+v.reason+'</td><td class="text-center">'+v.created_at+'</td></tr>';
+          })
+          $('table .history').html(html);
+        }else{
+          console.log("Notat");
+          $('.msge').css('display', 'block');
+        }
+
+      }
+    })
+  });
+  function editLocation(product_id){
+    var url = "{{route('inventory.edit.product.location', ':id')}}";
+    url = url.replace(':id', product_id);
+    $.ajax({
+      url: url,
+      type: 'GET',
+      cache: false,
+      headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+      // data: 'product_id='+product_id+'&status='+status,
+      success: function (data) {
+        $('#porduct_location_content').html(data);
+      }
+    });
+  }
+
+  function editInventory(product_id){
+    var url = "{{route('edit.inventory.product', ':id')}}";
+        url = url.replace(":id", product_id);
+    $.ajax({
+      url: url,
+      type: 'GET',
+      cache: false,
+      headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+      // data: 'product_id='+product_id+'&status='+status,
+      success: function (data) {
+        $('#edit_inventory_content').html(data);
+      }
+    });
+  }
+
+  function processPopupData(data,product_id){
+    var count_obj =  Object.keys(data).length;
+    var url = "{{route('inventory.print.pending.stock.label', ':id')}}";
+    url = url.replace(':id', product_id);
+    // var url = $('#frm_print').attr('data-url')+"/"+product_id
+    $('#frm_print').attr('action',url);
+    var content = "";
+    var size_det = data.products_sizes;
+    Object.keys(size_det).forEach(function(key) {
+      console.log(key, size_det[key]);
+      let row = size_det[key];
+      let type = "";
+      let size = "";
+      if( data.option_value > 0 ){
+        type = data.oms_options.option_name;
+        size = row.oms_option_details.value;
+      }else{
+        type = "Color";
+        size = data.option_name;
+      }
+      let style_center = 'align="center"';
+      let text_box = '<input type="text" name="print_quant['+product_id+']['+row.option_value_id+']" size="3" placeholder="Enter value to print" class="form-control">';
+      content += "<tr><td "+style_center+">"+type+"</td><td "+style_center+">"+size+"</td><td "+style_center+">"+row.available_quantity+"</td><td "+style_center+">"+text_box+"</td></tr>";
+
+
+    });
+    $('#printModal_content').html( content );
+
+  }
+  $('#frm_print').on('submit',function(){
+    $('#printModal').modal('toggle');
+  });
+
+</script>
+@endpush
