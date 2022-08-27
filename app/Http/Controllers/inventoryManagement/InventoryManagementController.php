b06bfc439ac075a76204119506e45be9bed7c695
Businessarcade <alisiraj346@gmail.com>
2022-08-27 15:15:31 +0400

    done

--- app/Http/Controllers/inventoryManagement/InventoryManagementController.php
+++ app/Http/Controllers/inventoryManagement/InventoryManagementController.php
@@ -1006,10 +1006,10 @@ use Carbon\Carbon;
   public function optionConnection(Request $request) {
     // $baOption = OptionDescriptionModel::groupBy('option_id')->where('language_id', 1)->orderBy('name')->get();
     // $dfOption = DressFairOptionDescriptionModel::groupBy('option_id')->where('language_id', 1)->orderBy('name')->get();
-    $oms_options = OmsOptions::with('omsOptionsDetails')->where('status',1)->groupBy('option_name')->orderBy('option_name')->get();
+    $oms_options = OmsOptions::with('omsOptionsDetails')->where('status',1)->groupBy('option_name')->orderBy('option_name')->select(DB::raw('MIN(option_name)'))->get();
     dd($oms_options);
     if($request->isMethod('post')) {
-      
+
 
     }
     return view(self::VIEW_DIR.".optionConnection")->with(compact('oms_options','baOption','dfOption'));
