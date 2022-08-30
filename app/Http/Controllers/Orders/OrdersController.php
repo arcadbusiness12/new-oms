55ac33f955a7e6e16a5e64531f8a97a8d1bf057b
nasir <nasir5074@gmail.com>
2022-08-30 12:08:05 +0400

    changes

--- app/Http/Controllers/Orders/OrdersController.php
+++ app/Http/Controllers/Orders/OrdersController.php
@@ -88,47 +88,61 @@ class OrdersController extends Controller
     // }
     public function index(){
         $old_input = Request::all();
+        // $data = DB::table("oms_orders AS ord")
+        // ->rightjoin($this->DB_BAOPENCART_DATABASE.".oc_order AS baord",function($join){
+        //   $join->on("baord.order_id","=","ord.order_id");
+        //   $join->on("ord.store","=",DB::raw("1"));
+        // })
+        // ->rightjoin($this->DB_DFOPENCART_DATABASE.".oc_order AS dford",function($join){
+        //     $join->on("dford.order_id","=","ord.order_id");
+        //     $join->on("ord.store","=",DB::raw("2"));
+        //   })
+        // ->orderBy('baord.date_added','DESC')->limit(100)->get();
+        // dd($data->toArray());
         $data = DB::table("oms_orders AS ord")
-              ->leftjoin($this->DB_BAOPENCART_DATABASE.".oc_order AS baord",function($join){
+              ->rightjoin($this->DB_BAOPENCART_DATABASE.".oc_order AS baord",function($join){
                 $join->on("baord.order_id","=","ord.order_id");
                 $join->on("ord.store","=",DB::raw("1"));
               })
-              ->leftjoin($this->DB_DFOPENCART_DATABASE.".oc_order AS dford",function($join){
+              ->rightjoin($this->DB_DFOPENCART_DATABASE.".oc_order AS dford",function($join){
                 $join->on("dford.order_id","=","ord.order_id");
                 $join->on("ord.store","=",DB::raw("2"));
               })
+              ->select(DB::raw("baord.order_id AS baordidd,dford.order_id AS dfordidd,
+                        (CASE WHEN baord.order_id IS NOT NULL THEN baord.order_id  ELSE dford.order_id END) AS order_id
+                "))
             // ->join("airwaybill_tracking AS awbt",function($join){
             //   $join->on('awbt.order_id','=','ord.order_id');
             //   $join->on('awbt.shipping_provider_id','=','ord.last_shipped_with_provider');
             //  })
-             ->join(DB::raw("(SELECT * FROM `airwaybill_tracking` WHERE tracking_id IN( SELECT MAX(`tracking_id`) FROM airwaybill_tracking GROUP BY order_id)) AS awbt"),function($join){
-              $join->on('awbt.order_id','=','ord.order_id');
-              $join->on('awbt.shipping_provider_id','=','ord.last_shipped_with_provider');
-             })
-             ->join("shipping_providers AS courier","courier.shipping_provider_id","=","ord.last_shipped_with_provider")
-             ->select(DB::raw("ord.order_id,ord.oms_order_status,ord.store,courier.name AS courier_name,awbt.airway_bill_number,awbt.courier_delivered,awbt.payment_status,awbt.created_at,
-                  (CASE WHEN ord.store = 1 THEN baord.total WHEN ord.store = 2 THEN dford.total ELSE 0 END) AS amount,
-                  (CASE WHEN ord.store = 1 THEN baord.payment_code WHEN ord.store = 2 THEN dford.payment_code ELSE 0 END) AS payment_code,
-                  (CASE WHEN ord.store = 1 THEN baord.shipping_address_1 WHEN ord.store = 2 THEN dford.shipping_address_1 ELSE 0 END) AS shipping_address_1,
-                  (CASE WHEN ord.store = 1 THEN baord.shipping_address_2 WHEN ord.store = 2 THEN dford.shipping_address_2 ELSE 0 END) AS shipping_address_2,
-                  (CASE WHEN ord.store = 1 THEN baord.shipping_area WHEN ord.store = 2 THEN dford.shipping_area ELSE 0 END) AS shipping_area,
-                  (CASE WHEN ord.store = 1 THEN baord.shipping_zone WHEN ord.store = 2 THEN dford.shipping_zone ELSE 0 END) AS shipping_zone,
-                  (CASE WHEN ord.store = 1 THEN baord.payment_address_1 WHEN ord.store = 2 THEN dford.payment_address_1 ELSE 0 END) AS payment_address_1,
-                  (CASE WHEN ord.store = 1 THEN baord.payment_address_2 WHEN ord.store = 2 THEN dford.payment_address_2 ELSE 0 END) AS payment_address_2,
-                  (CASE WHEN ord.store = 1 THEN baord.payment_area WHEN ord.store = 2 THEN dford.payment_area ELSE 0 END) AS payment_area,
-                  (CASE WHEN ord.store = 1 THEN baord.shipping_city WHEN ord.store = 2 THEN dford.shipping_city ELSE 0 END) AS shipping_city,
-                  (CASE WHEN ord.store = 1 THEN baord.firstname WHEN ord.store = 2 THEN dford.firstname ELSE 0 END) AS firstname,
-                  (CASE WHEN ord.store = 1 THEN baord.lastname WHEN ord.store = 2 THEN dford.lastname ELSE 0 END) AS lastname,
-                  (CASE WHEN ord.store = 1 THEN baord.telephone WHEN ord.store = 2 THEN dford.telephone ELSE 0 END) AS telephone,
-                  (CASE WHEN ord.store = 1 THEN baord.email WHEN ord.store = 2 THEN dford.email ELSE 0 END) AS email,
-                  (CASE WHEN ord.store = 1 THEN baord.total WHEN ord.store = 2 THEN dford.total ELSE 0 END) AS total,
-                  (CASE WHEN ord.store = 1 THEN baord.date_modified WHEN ord.store = 2 THEN dford.date_modified ELSE 0 END) AS date_modified,
-                  (CASE WHEN ord.store = 1 THEN baord.date_added WHEN ord.store = 2 THEN dford.date_added ELSE 0 END) AS date_added
-                "))
-            ->orderByRaw("CASE WHEN ord.store = 1 THEN baord.date_modified WHEN ord.store = 2 THEN dford.date_modified END DESC")
-            ->paginate(20);
+            //  ->leftjoin(DB::raw("(SELECT * FROM `airwaybill_tracking` WHERE tracking_id IN( SELECT MAX(`tracking_id`) FROM airwaybill_tracking GROUP BY order_id)) AS awbt"),function($join){
+            //   $join->on('awbt.order_id','=','ord.order_id');
+            //   $join->on('awbt.shipping_provider_id','=','ord.last_shipped_with_provider');
+            //  })
+            //  ->join("shipping_providers AS courier","courier.shipping_provider_id","=","ord.last_shipped_with_provider")
+            //  ->select(DB::raw("ord.order_id,ord.oms_order_status,ord.store,courier.name AS courier_name,awbt.airway_bill_number,awbt.courier_delivered,awbt.payment_status,awbt.created_at,
+            //       (CASE WHEN ord.store = 1 THEN baord.total WHEN ord.store = 2 THEN dford.total ELSE 0 END) AS amount,
+            //       (CASE WHEN ord.store = 1 THEN baord.payment_code WHEN ord.store = 2 THEN dford.payment_code ELSE 0 END) AS payment_code,
+            //       (CASE WHEN ord.store = 1 THEN baord.shipping_address_1 WHEN ord.store = 2 THEN dford.shipping_address_1 ELSE 0 END) AS shipping_address_1,
+            //       (CASE WHEN ord.store = 1 THEN baord.shipping_address_2 WHEN ord.store = 2 THEN dford.shipping_address_2 ELSE 0 END) AS shipping_address_2,
+            //       (CASE WHEN ord.store = 1 THEN baord.shipping_area WHEN ord.store = 2 THEN dford.shipping_area ELSE 0 END) AS shipping_area,
+            //       (CASE WHEN ord.store = 1 THEN baord.shipping_zone WHEN ord.store = 2 THEN dford.shipping_zone ELSE 0 END) AS shipping_zone,
+            //       (CASE WHEN ord.store = 1 THEN baord.payment_address_1 WHEN ord.store = 2 THEN dford.payment_address_1 ELSE 0 END) AS payment_address_1,
+            //       (CASE WHEN ord.store = 1 THEN baord.payment_address_2 WHEN ord.store = 2 THEN dford.payment_address_2 ELSE 0 END) AS payment_address_2,
+            //       (CASE WHEN ord.store = 1 THEN baord.payment_area WHEN ord.store = 2 THEN dford.payment_area ELSE 0 END) AS payment_area,
+            //       (CASE WHEN ord.store = 1 THEN baord.shipping_city WHEN ord.store = 2 THEN dford.shipping_city ELSE 0 END) AS shipping_city,
+            //       (CASE WHEN ord.store = 1 THEN baord.firstname WHEN ord.store = 2 THEN dford.firstname ELSE 0 END) AS firstname,
+            //       (CASE WHEN ord.store = 1 THEN baord.lastname WHEN ord.store = 2 THEN dford.lastname ELSE 0 END) AS lastname,
+            //       (CASE WHEN ord.store = 1 THEN baord.telephone WHEN ord.store = 2 THEN dford.telephone ELSE 0 END) AS telephone,
+            //       (CASE WHEN ord.store = 1 THEN baord.email WHEN ord.store = 2 THEN dford.email ELSE 0 END) AS email,
+            //       (CASE WHEN ord.store = 1 THEN baord.total WHEN ord.store = 2 THEN dford.total ELSE 0 END) AS total,
+            //       (CASE WHEN ord.store = 1 THEN baord.date_modified WHEN ord.store = 2 THEN dford.date_modified ELSE 0 END) AS date_modified,
+            //       (CASE WHEN ord.store = 1 THEN baord.date_added WHEN ord.store = 2 THEN dford.date_added ELSE 0 END) AS date_added
+            //     "))
+            ->orderByRaw("CASE WHEN baord.order_id IS NOT NULL THEN baord.date_added ELSE dford.date_added END DESC")
+            ->paginate(100);
             $data = $this->getOrdersWithImage($data);
-            // dd($data->toArray());
+            dd($data->toArray());
         ///
         $searchFormAction = URL::to('orders');
         $orderStatus = OrderStatusModel::all();
