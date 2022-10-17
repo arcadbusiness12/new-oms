00837043e59f4424583632ce9e973387e1753015
Businessarcade <alisiraj346@gmail.com>
2022-10-17 16:47:58 +0400

    done

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -229,6 +229,36 @@
                                 </li>
                             </ul>
                         </li>
+                        
+                        <li class="treeview @if( (str_contains(Request::url(), '/orders') && str_contains(Request::url(), '/PurchaseManagement') != 1) || ( str_contains(Request::url(), '/exchange') ) ) active @endif)">
+                            <a href="#">
+                                <i class="icon icon-shopping-cart s-24"></i> <span>Catalog</span>
+                                <i class=" icon-angle-left  pull-right"></i>
+                            </a>
+                            <ul class="treeview-menu">
+                                <li class="@if( str_contains(Request::url(), '/orders') && str_contains(Request::url(), '/PurchaseManagement') != 1 ) active @endif"><a href="#"><i class="icon icon-shopping-cart"></i>Attributes<i
+                                        class=" icon-angle-left  pull-right"></i></a>
+                                    <ul class="treeview-menu">
+                                        <li class="@if( Request::url() == route('orders') ) active @endif"><a href="{{ route('orders') }}">Attributes Group</a></li>
+                                        {{--  <li><a href="panel-page-blank-tabs.html">Customer Return Request </a></li>  --}}
+                                        <li class="@if( Request::url() == route('orders.online') ) active @endif"><a href="{{ route('orders.online') }}"> Attributes </a></li>
+                                        {{--  <li><a href="panel-page-blank-tabs.html">Ready For Returns </a></li>  --}}
+                                        <li class="@if( Request::url() == route('orders.picking-list-awaiting') ) active @endif"><a href="{{ route('orders.picking-list-awaiting') }}">Product Listing </a>
+                                        </li>
+
+                                    </ul>
+                                </li>
+                                <li><a href="#"><i class="icon icon-fingerprint text-green"></i>Reseller Orders<i
+                                        class=" icon-angle-left  pull-right"></i></a>
+                                    <ul class="treeview-menu">
+                                        <li><a href="login.html">Pending Orders</a>
+                                        </li>
+                                        <li><a href="login-2.html">Reaeller Return Request</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
 
                         <li class="treeview">
                             <a href="#">
