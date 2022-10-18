33e136246dd1e31b1a33b5c4bc4e75e607b31e47
nasir <nasir5074@gmail.com>
2022-10-18 10:33:25 +0400

    changes

--- app/Http/Controllers/Catalog/AttributeController.php
+++ app/Http/Controllers/Catalog/AttributeController.php
@@ -3,9 +3,21 @@
 namespace App\Http\Controllers\Catalog;
 
 use App\Http\Controllers\Controller;
+use App\Models\Oms\InventoryManagement\Attribute\AttributeModel;
+use App\Models\Oms\InventoryManagement\Attribute\AttributeGroupModel;
+use App\Models\Oms\InventoryManagement\Attribute\AttributePresetModel;
+use App\Models\Oms\InventoryManagement\Attribute\AttributeTemplateModel;
 use Illuminate\Http\Request;
 
 class AttributeController extends Controller
 {
-    //
+    public function attributeGroups(){
+
+    }
+    public function attributes(){
+
+    }
+    public function attributeTemplates(){
+
+    }
 }
